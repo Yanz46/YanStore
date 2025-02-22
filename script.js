959d407cd35c0802d20e787f5cc57039c343ae58
Yanz46 <narutoshipudhen97@gmail.com>
2025-02-22 22:04:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const userIdInput = document.getElementById("userId");
+const diamondAmountSelect = document.getElementById("diamondAmount");
+const topupButton = document.getElementById("topupButton");
+
+function updateWhatsappLink() {
+    const selectedAmount = diamondAmountSelect.value;
+    const userId = userIdInput.value;
+    if (selectedAmount && userId) {
+        const message = `Top-Up Free Fire - ID: ${userId} - ${selectedAmount} Diamonds`;
+        const encodedMessage = encodeURIComponent(message);
+        const whatsappLink = `https://6282229795661?text=${encodedMessage}`;
+        topupButton.href = whatsappLink;
+    } else {
+        topupButton.href = "#"; // Nonaktifkan tautan jika ID atau jumlah diamond kosong
+    }
+}
+
+diamondAmountSelect.addEventListener("change", updateWhatsappLink);
+userIdInput.addEventListener("input", updateWhatsappLink);
+
+// Panggil updateWhatsappLink saat halaman dimuat untuk inisialisasi
+updateWhatsappLink();
